bfdef18a9b624b23774acc1d995e51651a5674d8
Juan José Rendón <juanjoserendon13@outlook.com>
2019-10-18 11:42:56 -0500

    single quotes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const About = () => {
   return (
-    <div>
+    <Layout>
+      <SEO title="About" />
       <h1>About Us</h1>
       <p>Welcome to your new Gatsby site.</p>
-    </div>
+    </Layout>
   );
 };
 
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const Services = () => {
   return (
-    <div>
+    <Layout>
+      <SEO title="Services" />
       <h1>Services</h1>
       <p>Welcome to your new Gatsby site.</p>
-    </div>
+    </Layout>
   );
 };
 
